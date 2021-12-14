28b520745ce19066c6146fbfb7770c7266d8ece4
Brett Fleming <brettfleming015@gmail.com>
2021-12-13 20:22:38 -0600

    adding login and sign up forms

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,11 +1,24 @@
 import React, { useState, useEffect } from 'react';
+import { Form, Button, Alert } from 'react-bootstrap';
 
 function loginForm() {
   
     
     return (
         <div>
-            
+            <div>
+                <div>
+                    <h2>Login</h2>
+                    
+
+                </div>
+                <div>
+                    <h2>Sign up</h2>
+
+                </div>
+            </div>
         </div>
     )
 }
+
+export default loginForm;
