29bf555e66e28fcce5471e7c5356f48c25f75784
Brett Fleming <brettfleming015@gmail.com>
2021-12-14 20:48:08 -0600

    removed unneeded files

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -18,15 +18,6 @@ export const getFeed = () => {
   },
 });
 };
-
-export const getColonies = () => {
-  return fetch('/api/colonies', {
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
-};
-
 export const createUser = (userData) => {
   return fetch('/api/users', {
     method: 'POST',
@@ -55,15 +46,6 @@ export const addPostToUser = (data) => {
   });
 };
 
-// export const updateUser = (userData) => {
-//   return fetch('/api/users', {
-//     method: 'PUT',
-//     headers: {
-//       'Content-Type': 'application/json',
-//     },
-//     body: JSON.stringify(userData),
-//   });
-// };
 
 export const loginUser = (userData) => {
   return fetch('/api/users/login', {
@@ -94,22 +76,6 @@ export const deletePost = (postId, token) => {
     },
   });
 };
-
-export const searchAPOD = () => {
-  return fetch('https://api.nasa.gov/planetary/apod?api_key=v1vqd0PdgPpMhpKjaxObGA6dQtpp5g5KweeYDx7O')
-};
-
-export const searchRoverImg = () => {
-  return fetch('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=v1vqd0PdgPpMhpKjaxObGA6dQtpp5g5KweeYDx7O')
-}
-
-// make a search to open weather api
-// https://api.openweathermap.org/data/2.5/weather?q= {los+angeles} &appid=9b2ae69bfce6899c26e740f85827a619 
-export const searchWeatherApi = (query) => {
-  const apiKey = '9b2ae69bfce6899c26e740f85827a619'
-  return fetch(`https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${apiKey}`);
-};
-
 
 
 
