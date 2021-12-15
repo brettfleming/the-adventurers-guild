a308d576b6e9da90b87265cbd0f4c311ca82a14f
Brett Fleming <brettfleming015@gmail.com>
2021-12-14 21:11:20 -0600

    flushed out the sharacter model more

--- server/models/Characters.js
+++ server/models/Characters.js
@@ -1,6 +1,33 @@
 const { Schema, model } = require('mongoose');
 
 const characterSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        age: {
+            type: Number,
+        },
+        class: {
+            type: String,
+            required: true,
+        },
+        race: {
+            type: String,
+            required: true,
+        },
+        level: {
+            type: Number,
+            required: true,
+        },
+        background: {
+            type: String
+        },
+        skills: {
+            type: String,
+        }
+    }
 
 
 );
