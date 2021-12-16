deded96d2c188c6de0dd5acf46e01b227ffabf48
Brett Fleming <brettfleming015@gmail.com>
2021-12-15 19:05:24 -0600

    adding the handler for the form

--- client/src/pages/createCharacter.js
+++ client/src/pages/createCharacter.js
@@ -24,6 +24,36 @@ const CreateCharacter = () => {
         }
       )
   }, [])
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    // check if form has everything (as per react-bootstrap docs)
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    try {
+      const response = await loginUser(userFormData);
+
+      if (!response.ok) {
+        throw new Error('something went wrong!');
+      }
+
+      const { token, user } = await response.json();
+      console.log(user);
+      Auth.login(token);
+    } catch (err) {
+      console.error(err);
+      setShowAlert(true);
+    }
+
+    setUserFormData({
+      username: '',
+      email: '',
+      password: '',
+    });
   console.log(apiData);
 
   if (error) {
@@ -33,7 +63,7 @@ const CreateCharacter = () => {
   } else {
     return (
         <>
-        <Form></Form>
+        <Form noValidate validated={validated} onSubmit={handleFormSubmit}></Form>
         </>
     )
   }
